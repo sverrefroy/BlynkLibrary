973de376859a4e9787ea5fb0cad7edc0dfdb5a82
Anders Frisk <andersfrisk77@gmail.com>
2018-10-11 14:14:56 +0200

    test server

--- BlynkTester/Program.cs
+++ BlynkTester/Program.cs
@@ -1,15 +1,44 @@
 ﻿using BlynkMinimalLibrary;
 using System;
 using System.Threading.Tasks;
+using PowerArgs;
 
 namespace BlynkTester
 {
+    [ArgExceptionBehavior(ArgExceptionPolicy.StandardExceptionHandling)]
+    public class Options
+    {
+        [ArgShortcut("s")]
+        [ArgShortcut("--server")]
+        [ArgDefaultValue("tcp://127.0.0.1:8080")]
+        [ArgDescription("The url to the Blynk server.")]
+        public string Server { get; set; }
+
+        [ArgShortcut("a")]
+        [ArgShortcut("--authorization")]
+        [ArgDefaultValue("****")]
+        [ArgDescription("The device authorization token used to identitify the repeater.")]
+        public string Authorization { get; set; }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var url = "tcp://192.168.1.210:8080"; // Local Blynk server 
-            var authorization = "d634a89dbdcf4c8a8f6e5fa9df96fed2"; // Authorization token
+            Options options = null;
+            try
+            {
+               options = Args.Parse<Options>(args);
+            }
+            catch (ArgException e)
+            {
+              Console.WriteLine(string.Format("Problems with the command line options: {0}", e.Message));
+              Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<Options>());
+              return;
+            }
+            var url = options.Server; // Blynk server address
+            var authorization = options.Authorization; // Authorization token
             using (var client = new Client(url, authorization))
             {
                 client.Connect();
@@ -31,13 +60,19 @@ namespace BlynkTester
                         Console.WriteLine($"Read Pin {id}");
                         client.WriteVirtualPin(id, counter++);
                     };
-
+                    var closeTcs = new System.Threading.Tasks.TaskCompletionSource<bool>();
+                    Console.CancelKeyPress += (o, e) =>
+                    {
+                      closeTcs.SetResult(true);
+                    };
+                    Console.WriteLine("Test server is active. Press CTRL+C to stop.");
+                    closeTcs.Task.Wait();
+                    Console.WriteLine("Stopping Test server.");
                 }
                 else
                 {
                     Console.WriteLine("Cannot authorize client with given token.");
                 }
-                Console.ReadKey();
                 client.Disconnect();
             }
         }
